81bca0ff0c7081c1bf5687c47325a8a8d90c75df
Ioannis Tsakpinis <iotsakp@gmail.com>
2014-11-24 00:38:00 +0200

    OSX: getpid

---
+++ java/org/lwjgl/system/macosx/Unistd.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright LWJGL. All rights reserved.
+ * License terms: http://lwjgl.org/license.php
+ * MACHINE GENERATED FILE, DO NOT EDIT
+ */
+package org.lwjgl.system.macosx;
+
+import org.lwjgl.*;
+import org.lwjgl.system.*;
+
+import static org.lwjgl.system.Checks.*;
+
+/** Native bindings to <unistd.h>. */
+public final class Unistd {
+
+	static { Sys.touch(); }
+
+	private Unistd() {}
+
+	// --- [ getpid ] ---
+
+	/** Returns the process ID of the calling process. */
+	public static native long getpid();
+
+}
