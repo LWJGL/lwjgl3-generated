79d297a0d9ec9e97019c1caaa9781e2762ead4a0
Ioannis Tsakpinis <iotsakp@gmail.com>
2016-04-07 19:11:02 +0300

    Add VK_IMG_filter_cubic

---
+++ java/org/lwjgl/vulkan/IMGFilterCubic.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright LWJGL. All rights reserved.
+ * License terms: http://lwjgl.org/license.php
+ * MACHINE GENERATED FILE, DO NOT EDIT
+ */
+package org.lwjgl.vulkan;
+
+/** The IMG_filter_cubic extension. */
+public final class IMGFilterCubic {
+
+	/** The extension specification version. */
+	public static final int VK_IMG_FILTER_CUBIC_SPEC_VERSION = 1;
+
+	/** The extension name. */
+	public static final String VK_IMG_FILTER_CUBIC_EXTENSION_NAME = "VK_IMG_filter_cubic";
+
+	/** VkFilter */
+	public static final int VK_FILTER_CUBIC_IMG = 0x3B9B0498;
+
+	/** VkFormatFeatureFlagBits */
+	public static final int VK_FORMAT_FEATURE_SAMPLED_IMAGE_FILTER_CUBIC_BIT_IMG = 0x2000;
+
+	private IMGFilterCubic() {}
+
+}
