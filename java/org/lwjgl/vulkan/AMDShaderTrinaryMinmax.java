d64d7b87a848dab190ca4a80bdb734482cfa70b6
Ioannis Tsakpinis <iotsakp@gmail.com>
2016-06-03 13:55:49 +0300

    Update AMD_shader_trinary_minmax documentation

--- java/org/lwjgl/vulkan/AMDShaderTrinaryMinmax.java
+++ java/org/lwjgl/vulkan/AMDShaderTrinaryMinmax.java
@@ -9,8 +9,27 @@ package org.lwjgl.vulkan;
  * When the {@code VK_AMD_shader_trinary_minmax} device extension is enabled the application <b>can</b> pass a SPIR-V module to {@link VK10#vkCreateShaderModule CreateShaderModule} that uses
  * the {@code SPV_AMD_shader_trinary_minmax} SPIR-V extension.
  * 
- * <p>This extension introduces 3-parameter {@code min}, {@code max} and {@code med} functions which map directly to the corresponding GCN opcodes:
- * {@code min3}, {@code max3}, {@code med3}</p>
+ * <p>This extension is written to provide the functionality of the {@code AMD_shader_trinary_minmax}, OpenGL Shading Language Specification extension, for
+ * SPIR-V.</p>
+ * 
+ * <p>This extension introduces nine new trinary extended instructions to SPIR-V. These functions allow the minimum, maximum or median of three inputs to be
+ * found with a single function call. These operations may be useful for sorting and filtering operations, for example. By explicitly performing a trinary
+ * operation with a single built-in function, shader compilers and optimizers may be able to generate better instruction sequences to perform sorting and
+ * to other multi-input functions.</p>
+ * 
+ * <p>This extension adds the following extended instructions:</p>
+ * 
+ * <ul>
+ * <li>FMin3AMD</li>
+ * <li>UMin3AMD</li>
+ * <li>SMin3AMD</li>
+ * <li>FMax3AMD</li>
+ * <li>UMax3AMD</li>
+ * <li>SMax3AMD</li>
+ * <li>FMid3AMD</li>
+ * <li>UMid3AMD</li>
+ * <li>SMid3AMD</li>
+ * </ul>
  */
 public final class AMDShaderTrinaryMinmax {
 
