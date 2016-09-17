83b9c8fc1aa1653d0aadb6eb8e4d628e3585ac0c
Ioannis Tsakpinis <iotsakp@gmail.com>
2016-09-17 19:15:29 +0300

    Update jemalloc project URLs

--- java/org/lwjgl/system/jemalloc/package-info.java
+++ java/org/lwjgl/system/jemalloc/package-info.java
@@ -5,10 +5,10 @@
  */
 
 /**
- * Contains bindings to the <a href="http://www.canonware.com/jemalloc/">jemalloc</a> library. jemalloc is a general purpose malloc implementation that
- * emphasizes fragmentation avoidance and scalable concurrency support.
+ * Contains bindings to the <a href="http://jemalloc.net/">jemalloc</a> library. jemalloc is a general purpose malloc implementation that emphasizes
+ * fragmentation avoidance and scalable concurrency support.
  * 
- * <p>The jemalloc documentation can be found <a href="http://www.canonware.com/download/jemalloc/jemalloc-latest/doc/jemalloc.html">here</a>. The jemalloc
+ * <p>The jemalloc documentation can be found <a href="http://jemalloc.net/jemalloc.3.html">here</a>. The jemalloc
  * <a href="https://github.com/jemalloc/jemalloc/wiki">wiki</a> also contains useful information.</p>
  * 
  * <p>The jemalloc shared library that comes with LWJGL is configured with:</p>
@@ -25,8 +25,7 @@
  * <p>The shared library may be replaced with a custom build that has more features enabled.</p>
  * 
  * <p>Dynamic configuration (for enabled features) is also possible, using either the {@code MALLOC_CONF} environment variable or the
- * <a href="http://www.canonware.com/download/jemalloc/jemalloc-latest/doc/jemalloc.html#mallctl_namespace">MALLCTL NAMESPACE</a> and the
- * {@code mallctl*} functions.</p>
+ * <a href="http://jemalloc.net/jemalloc.3.{@link html#mallctl_namespace}">MALLCTL NAMESPACE</a> and the {@code mallctl*} functions.</p>
  */
 package org.lwjgl.system.jemalloc;
 
