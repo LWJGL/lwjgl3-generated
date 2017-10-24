8b6178e3944e2ee115644e0e69ee8def8b5d13e6
Ioannis Tsakpinis <iotsakp@gmail.com>
2017-10-24 22:16:49 +0300

    feat(xxhash): update to latest version

--- java/org/lwjgl/util/xxhash/XXHash.java
+++ java/org/lwjgl/util/xxhash/XXHash.java
@@ -184,8 +184,7 @@ public class XXHash {
      * Default result type for XXH functions are primitive unsigned 32 and 64 bits.
      * 
      * <p>The canonical representation uses human-readable write convention, aka big-endian (large digits first). These functions allow transformation of hash
-     * result into and from its canonical format. This way, hash values can be written into a file / memory, and remain comparable on different systems and
-     * programs.</p>
+     * result into and from its canonical format. This way, hash values can be written into a file / memory, remaining comparable across different systems.</p>
      *
      * @param dst  the destination canonical representation
      * @param hash the source hash
