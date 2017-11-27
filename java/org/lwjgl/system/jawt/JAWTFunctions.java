da00612c68733f5d2106981c6aa639a6e99d4bfb
Ioannis Tsakpinis <iotsakp@gmail.com>
2017-11-27 16:46:17 +0200

    feat(jawt): add library name override

--- java/org/lwjgl/system/jawt/JAWTFunctions.java
+++ java/org/lwjgl/system/jawt/JAWTFunctions.java
@@ -65,7 +65,7 @@ public class JAWTFunctions {
         throw new UnsupportedOperationException();
     }
 
-    private static final SharedLibrary JAWT = Library.loadNative(JAWTFunctions.class, "jawt");
+    private static final SharedLibrary JAWT = Library.loadNative(JAWTFunctions.class, Configuration.JAWT_LIBRARY_NAME.get("jawt"));
 
     /** Contains the function pointers loaded from the jawt {@link SharedLibrary}. */
     public static final class Functions {
