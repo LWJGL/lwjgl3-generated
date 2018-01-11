fad255ee90166d39014e4c99ac1f359666d01c3d
Ioannis Tsakpinis <iotsakp@gmail.com>
2018-01-11 14:49:20 +0200

    build(lmdb): enable MDB_VL32 on 32-bit

--- c/util/lmdb/org_lwjgl_util_lmdb_LMDB.c
+++ c/util/lmdb/org_lwjgl_util_lmdb_LMDB.c
@@ -11,6 +11,9 @@ DISABLE_WARNINGS()
 #ifdef LWJGL_WINDOWS
     __pragma(warning(disable : 4172 4701 4706))
 #endif
+#ifdef LWJGL_x86
+    #define MDB_VL32 1
+#endif
 #define MDB_DEVEL 2
 #include "lmdb.h"
 ENABLE_WARNINGS()
